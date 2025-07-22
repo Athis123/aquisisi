261928268313068fb25d94dba74ad65ff62bba40
mhmdkahfi21 <mhmdkahfi21@gmail.com>
2025-07-22 20:48:18 +0700

    init

--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -1,8 +1,28 @@
-@extends('layouts.app')
+@extends('layouts.stisla')
+
+@section('title', 'Dashboard Admin')
 
 @section('content')
-<div class="container">
+  <div class="section-header">
     <h1>Dashboard Admin</h1>
+  </div>
+
+  <div class="section-body">
     <p>Selamat datang, {{ Auth::user()->name }}</p>
-</div>
+
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-sm-12">
+        <div class="card card-statistic-2">
+          <div class="card-stats">
+            <div class="card-stats-title">
+              Order Statistics
+              <div class="dropdown d-inline">
+                ...
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,42 +15,57 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Login Form
+Route::get('/', [LoginController::class, 'index'])->name('login');
+
+// Auth route
+Route::prefix('auth')->as('auth.')->group(function () {
+    Route::post('/authenticate', [LoginController::class, 'authenticate'])->name('authenticate');
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 });
 
-Route::get('/dashboard', function () {
-    $user = Auth::user();
+// Administrator
+Route::group(['middleware' => ['auth'],'prefix' => 'administrator', 'as' => 'admin.'], function(){
 
-    if ($user->hasRole('admin')) {
-        return redirect()->route('admin.dashboard');
-    } elseif ($user->hasRole('operator')) {
-        return redirect()->route('operator.dashboard');
-    } elseif ($user->hasRole('user')) {
-        return redirect()->route('user.dashboard');
-    }
-
-    // fallback
-    abort(403, 'Role not assigned.');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware(['auth', 'role:admin'])->group(function () {
-    Route::get('/admin/dashboard', function () {
-        return view('dashboard.admin');
-    })->name('admin.dashboard');
+    // Dashboard
+    Route::group(['namespace' => 'App\Http\Controllers\Dashboard','prefix' => 'dashboard', 'as' => 'dashboard.'], function(){
+        Route::get('/','DashboardController@index')->name('index');
+        Route::get('/tasks/priority-data','DashboardController@getTasksByPriority')->name('priority');
+    });
 });
 
-Route::middleware(['auth', 'role:operator'])->group(function () {
-    Route::get('/operator/dashboard', function () {
-        return view('dashboard.operator');
-    })->name('operator.dashboard');
-});
+// Route::get('/dashboard', function () {
+//     $user = Auth::user();
 
-Route::middleware(['auth', 'role:user'])->group(function () {
-    Route::get('/user/dashboard', function () {
-        return view('dashboard.user');
-    })->name('user.dashboard');
-});
+//     if ($user->hasRole('admin')) {
+//         return redirect()->route('admin.dashboard');
+//     } elseif ($user->hasRole('operator')) {
+//         return redirect()->route('operator.dashboard');
+//     } elseif ($user->hasRole('user')) {
+//         return redirect()->route('user.dashboard');
+//     }
+
+//     // fallback
+//     abort(403, 'Role not assigned.');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware(['auth', 'role:admin'])->group(function () {
+//     Route::get('/admin/dashboard', function () {
+//         return view('dashboard.admin');
+//     })->name('admin.dashboard');
+// });
+
+// Route::middleware(['auth', 'role:operator'])->group(function () {
+//     Route::get('/operator/dashboard', function () {
+//         return view('dashboard.operator');
+//     })->name('operator.dashboard');
+// });
+
+// Route::middleware(['auth', 'role:user'])->group(function () {
+//     Route::get('/user/dashboard', function () {
+//         return view('dashboard.user');
+//     })->name('user.dashboard');
+// });
 
 
 Route::middleware('auth')->group(function () {
